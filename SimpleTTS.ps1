aa455ecfb26ff04025dd8078f99ca4f842cb0b80
Randall Borck <rborck@navexglobal.com>
2019-04-12 14:00:17 -0400

    Added a simple TTS script that reads the text on the clipboard out loud

---
+++ SimpleTTS.ps1
@@ -0,0 +1,11 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$text = Get-Clipboard
+
+if ($text.Length -gt 0) {
+    $speak.Speak($text)
+} else {
+    write "Else statement..."
+    $result = $speak.SpeakAsync("There is no text on the clipboard right now. Try to copy something and then run Read-It.")
+}
+
